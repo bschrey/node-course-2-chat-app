a19e5444a4717e6e39451f8d2982edffcb791b6c
Brett Schrey <brettschrey@gmail.com>
2018-09-15 17:39:14 -0500

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -60,8 +60,7 @@ jQuery('#message-form').on('submit', function(e) {
 	e.preventDefault();
 	var messageTextbox = jQuery('[name=message]');
 	socket.emit('createMessage', {
-		from: 'User',
-		text: jQuery('[name=message]').val()
+		text: messageTextbox.val()
 	}, function() {
 		messageTextbox.val('');
 	});
