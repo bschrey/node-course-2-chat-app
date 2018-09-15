3d2511a6d61bf57c9a7d9d19a1e84f423a33cddf
Brett Schrey <brettschrey@gmail.com>
2018-09-14 21:39:06 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'brett',
-		text: 'Hey. This is brett.'
-	});
 });
 
 socket.on('disconnect', function() {
